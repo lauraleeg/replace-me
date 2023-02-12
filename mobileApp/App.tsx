e1b1a71652dacf6c22da0e7ce6f1f67c66b3f964
= <=>
2023-02-12 00:48:22 -0400

    prettier

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  Text,
+	SafeAreaView,
+	ScrollView,
+	Text,
 } from 'react-native';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const ScreenContainer = styled(SafeAreaView)`
   height: 100%;
 `;
 
 function App() {
-  const test = "testing my lint";
-  return (
-    <ScreenContainer>
-      <ScrollView>
-        <Text>Hello World</Text>
-      </ScrollView>
-    </ScreenContainer>
-  );
+	return (
+		<ScreenContainer>
+			<ScrollView>
+				<Text>Hello World</Text>
+			</ScrollView>
+		</ScreenContainer>
+	);
 }
 
 export default App;
